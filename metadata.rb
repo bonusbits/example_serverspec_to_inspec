b580fbf0c7c4e9fea13a19c8af9e631e4b1c934b
Levon Becker <levon.becker.github@bonusbits.com>
2017-04-17 10:16:35 -0700

    Fixed license and updated descriptions [skip ci]

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'example_serverspec_to_inspec'
 maintainer 'Levon Becker'
 maintainer_email 'levon.becker.github@bonusbits.com'
-license 'All Rights Reserved'
-description 'Installs/Configures example_serverspec_to_inspec'
-long_description 'Installs/Configures example_serverspec_to_inspec'
+license 'MIT'
+description 'Demonstrate Migration Integration Tests from ServerSpec to InSpec'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 chef_version '~> 12.5' if respond_to?(:chef_version)
 issues_url 'https://github.com/bonusbits/example_serverspec_to_inspec/issues'
